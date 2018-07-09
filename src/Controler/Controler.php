e923e18c3c9a2c0fd854b5ac42fb6057feedfad0
ChristelleDrgt <christelle31140@hotmail.fr>
2018-07-09 16:04:46 +0200

    Project structure

---
+++ src/Controler/Controler.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Candidature\Controler;
+
+
+class Controler {
+    public function parseCsv($csv){
+
+    }
+
+    public function sendJson($json){
+
+    }
+
+    public function getFolder(){
+
+    }
+
+    public function getUser(){
+
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,21 @@
+<?php
+    namespace Candidature\Entity;
+
+class User {
+    private $identifier;
+    private $folder;
+
+    /**
+     * @return mixed
+     */
+    public function getIdentifier() {
+        return $this->identifier;
+    }
+
+    /**
+     * @param mixed $identifier
+     */
+    public function setIdentifiant($identifier) {
+        $this->identifier = $identifier;
+    }
+}
