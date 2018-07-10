69636ec1eb8eaf05bace260c26fae2bb7317e18d
Aurelien <aurelien.latour@hotmail.fr>
2018-07-10 14:40:38 +0200

    test

--- src/DAO/FolderDAO.php
+++ src/DAO/FolderDAO.php
@@ -2,16 +2,35 @@
 
 namespace Candidature\DAO;
 
+use Candidature\Entity\Folder;
 
 class FolderDAO {
     private $database;
     private $folder;
 
-    public function insertData($json){
-
+    /**
+     * FolderDAO constructor.
+     * @param $database
+     */
+    public function __construct($database)
+    {
+        $this->database = $database;
     }
 
-    public function getData(){
 
+    public function insertData($folder)
+    {
+        $db = $this->database;
+        $q = $db->prepare('INSERT INTO folder VALUES (:lastname, :firstname, :folderCreation, :career, :phoneNumber, :mail, :state)');
+        $q->bindValue(':lastname', $folder->)
     }
+
+    public function getData()
+    {
+        $db = $this->database;
+        $q = $db->prepare('SELECT * FROM folder');
+        $q->execute();
+    }
+
+
 }
