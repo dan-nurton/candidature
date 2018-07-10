3ce23c88cd18f05be6e251941550ed728c75e846
Aurelien <aurelien.latour@hotmail.fr>
2018-07-10 11:24:16 +0200

    remove .idea/workspace.xml

---
+++ src/DAO/StateDAO.php
@@ -0,0 +1,15 @@
+<?php
+    namespace Candidature\DAO;
+
+class StateDAO {
+    private $database;
+    private $state;
+
+    public function getState(){
+
+    }
+
+    public function updateState($key, $value){
+
+    }
+}
---
+++ src/Utils/ConnectBdd.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Christelle
+ * Date: 09/07/2018
+ * Time: 16:02
+ */
+
+namespace Candidature\Utils;
+
+
+class ConnectBdd {
+    public function connect(){
+        
+    }
+}
